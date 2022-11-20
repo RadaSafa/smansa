3539621c78695245d4de25d51b7d7429c842ea56
RADA <rada@RADAs-MacBook-Air.local>
2022-11-21 02:27:54 +0700

    feat : add news

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,22 +3,31 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 
 import { LoginComponent } from './login/login.component';
+import { NewsComponent } from './news/news.component';
 
 const routes: Routes = [
   {
-    path :'home', component:HomeComponent
+    path: 'home',
+    component: HomeComponent,
   },
- 
+
   {
-    path:'login', component:LoginComponent
+    path: 'login',
+    component: LoginComponent,
   },
   {
-    path:'', redirectTo:'home', pathMatch:'full'
-  }
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'news',
+    component: NewsComponent,
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
